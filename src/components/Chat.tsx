ffce0498fd648c807bcc08d2ad8c952b30b64780
sofikin <sofia.kalpin@khk.ee>
2022-02-14 12:29:26 +0200

    "Added some styling and a data file"

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Chat.css";
+import data from "../data.json";
 
 function Chat() {
   return (
     <div>
       <h1>KHK</h1>
       <div className="chatBox">
+        <div className="chatContainer">Lorem Ipsum something</div>
         <div className="chatInputBox">
           <div className="username">Guest3367</div>
-          <div className="input">Input text</div>
+          <div className="input">Input textnpm</div>
         </div>
       </div>
       <div className="btn">Send</div>
