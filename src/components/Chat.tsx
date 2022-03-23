71acf032cca38e37115f303c26c4e67619ca963b
sofikin <sofia.kalpin@khk.ee>
2022-03-23 21:35:56 +0200

    "Used React Hooks to Get Input Value"

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import data from "../data.json";
 import dot from "./Oval.svg";
 
 function Chat() {
+  const [username, setUsername] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const messages = [username, message];
+
+  const handleInput = () => {
+    console.log(username, message, messages);
+  };
   return (
     <div className="chat">
       <h1>KHK</h1>
@@ -26,15 +33,28 @@ function Chat() {
       <div className="chatInputBox">
         <form>
           <div className="username">
-            <input type="username" placeholder="Username" required />
+            <input
+              type="username"
+              name="username"
+              placeholder="Username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="input">
-            <input type="text" placeholder="Type your message.." required />
+            <input
+              type="text"
+              name="text"
+              placeholder="Type your message.."
+              required
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </div>
           <div className="btn">
-            <input type="submit" value="Submit" />
+            <button onClick={() => handleInput()}>Input</button>
           </div>
         </form>
+        {username ? <p>Welcome {username}!</p> : " "}
       </div>
     </div>
   );
